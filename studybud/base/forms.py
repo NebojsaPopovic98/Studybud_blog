944dec009927dcdbdf6e44ff41a60ee1bb1eb493
NebojsaPopovic98 <nebojsap1998@gmail.com>
2021-12-20 18:28:22 +0100

    save

---
+++ studybud/base/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import Room
+from django.contrib.auth.models import User
+
+
+class Room_form(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+class User_form(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
